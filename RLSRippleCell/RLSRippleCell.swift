1bf135bc4c603b8d3bb028306027c5157b949438
n-iwabuchi <n_iwabuchi@waku-2.com>
2015-08-20 13:12:58 +0900

    first commit

---
+++ RLSRippleCell/RLSRippleCell.swift
@@ -0,0 +1,129 @@
+//
+//  RLSRippleCell.swift
+//  
+//
+//  Created by Nari on 2015/08/19.
+//
+//
+
+
+import UIKit
+
+class RLSRippleCell: UITableViewCell {
+    var targetColor: UIColor!
+    var targetBGColor: UIColor!
+    var showDuration: NSTimeInterval!
+    private var circleView: UIView!
+    private var touchedPoint: CGPoint!
+    private var bgView:UIView!
+    var radiusSmallLength: CGFloat!
+    var radiusLeargeLength: CGFloat!
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func setHighlighted(highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        touchedPoint = touch.locationInView(self)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+        super.touchesCancelled(touches, withEvent: event)
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        animationWithCircleStart()
+        super.touchesEnded(touches, withEvent: event)
+    }
+    
+    func setLayout(){
+        targetColor = UIColor(red: 101.0/255.0, green: 198.0/255.0, blue: 187.0/255.0, alpha: 0.9)
+        targetBGColor = UIColor(red: 200.0/255.0, green: 247.0/255.0, blue: 197.0/255.0, alpha: 1.0)
+        bgView = UIView(frame: CGRectMake(0, 0, self.frame.size.width, self.frame.size.height))
+        bgView.clipsToBounds = true
+        bgView.layer.masksToBounds = true
+        backgroundView = bgView
+        self.textLabel?.backgroundColor = UIColor.clearColor()
+        touchedPoint = self.contentView.center
+        showDuration = 0.6
+        self.layer.masksToBounds = true
+        radiusSmallLength = self.frame.size.height * 5
+        radiusLeargeLength = self.frame.size.width * 10
+    }
+    
+    func animationWithCircleStart(){
+        showCircleView()
+        showFadeBackgroundIn()
+    }
+    
+    func animationWithCirleEnd(){
+        self.removeFadeBackgroundOut()
+        self.removeCircleView()
+    }
+    
+    func showCircleView(){
+        circleView = UIView(frame: CGRectMake(0, 0, radiusLeargeLength, radiusLeargeLength))
+        circleView.center = touchedPoint
+        circleView.layer.cornerRadius = radiusLeargeLength / 2
+        circleView.layer.masksToBounds = true
+        circleView.backgroundColor = targetColor
+        self.selectedBackgroundView.addSubview(circleView)
+        
+        circleView.transform = CGAffineTransformMakeScale(0, 0)
+        UIView.animateWithDuration(showDuration,
+            delay: 0,
+            options: UIViewAnimationOptions.CurveEaseIn,
+            animations: { () -> Void in
+                self.circleView.center = self.touchedPoint
+                self.circleView.transform = CGAffineTransformIdentity
+            }, completion: nil)
+        
+    }
+    
+    func removeCircleView(){
+        UIView.animateWithDuration(showDuration,
+            delay: 0, options: UIViewAnimationOptions.CurveEaseIn,
+            animations: { () -> Void in
+                self.circleView.alpha = 0.0
+                self.circleView.layer.cornerRadius = self.frame.size.width / 3
+                self.circleView.frame = CGRectMake(self.touchedPoint.x - self.radiusLeargeLength / 2, self.touchedPoint.y - self.radiusLeargeLength / 2, self.radiusLeargeLength, self.radiusLeargeLength)
+            }) { (animated) -> Void in
+                self.circleView.removeFromSuperview()
+        }
+    }
+    
+    func showFadeBackgroundIn(){
+        bgView.backgroundColor = targetBGColor
+        bgView.alpha = 0.0
+        UIView.animateWithDuration(showDuration, animations: { () -> Void in
+            self.bgView.alpha = 1.0
+            }) { (animated) -> Void in
+                self.animationWithCirleEnd()
+        }
+    }
+    
+    func removeFadeBackgroundOut(){
+        self.bgView.alpha = 0.0
+    }
+    
+}
---
+++ RLSRippleCellTests/RLSRippleCellTests.swift
@@ -0,0 +1,36 @@
+//
+//  RLSRippleCellTests.swift
+//  RLSRippleCellTests
+//
+//  Created by 岩渕響聖 on 2015/08/19.
+//  Copyright (c) 2015年 nari. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RLSRippleCellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
