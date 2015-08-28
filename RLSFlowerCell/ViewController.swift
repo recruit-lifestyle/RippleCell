8d1a71d5640dbae7aa0d69a140d97a8e4119d051
n-iwabuchi <n_iwabuchi@waku-2.com>
2015-08-28 12:53:45 +0900

    create RLSFlowerCell

---
+++ RLSFlowerCell/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  RLSFlowerCell
+//
+//  Created by nari on 2015/06/29.
+//  Copyright (c) 2015年 Nari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(RLSRippleCell.self, forCellReuseIdentifier: "MyCell")
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Num: \(indexPath.row)")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 100.0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as! RLSRippleCell
+        cell.selectedBackgroundView = UIView()
+        cell.selectedBackgroundView.backgroundColor = UIColor.clearColor()
+        cell.textLabel!.text = " Touch Me "
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ RLSFlowerCellTests/RLSFlowerCellTests.swift
@@ -0,0 +1,36 @@
+//
+//  RLSFlowerCellTests.swift
+//  RLSFlowerCellTests
+//
+//  Created by nari on 2015/06/29.
+//  Copyright (c) 2015年 Nari. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RLSFlowerCellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
